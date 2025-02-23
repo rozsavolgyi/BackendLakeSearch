346f9a22f2138b98a7d1c96fa36e237e466842b0
rozsavolgyi <simonrozsavolgyi5@gmail.com>
2025-02-23 20:32:49 +0100

    jelszó titkosítása

--- models/User.js
+++ models/User.js
@@ -30,3 +30,11 @@ const UserSchema = new mongoose.Schema({
     }
 
 })
+// A jelszó titkosítása bcrypt-tel
+UserSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        next()
+    }
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
