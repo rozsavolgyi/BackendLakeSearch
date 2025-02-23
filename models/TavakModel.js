cb0e08e7d370c83ffc72bc388540acd8c66a9be9
rozsavolgyi <simonrozsavolgyi5@gmail.com>
2025-02-23 21:05:20 +0100

    tavakModel létrehozása

---
+++ models/TavakModel.js
@@ -0,0 +1,79 @@
+const mongoose = require("mongoose");
+const ToSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    // required: [true, "Please add a name"],
+    // unique: true,
+    // trim: true,
+    // maxlength: [50, "Name can not be more than 50 characters!"],
+  },
+  img: {
+    type: String,
+    default: "no-photo.jpg",
+  },
+  location: {
+    type: String
+  },
+  coordinates:{
+    type: [Number]
+  },
+  description:{
+    type:String
+  },
+  typical_fish: {
+    type: [String],
+    required: true,
+    enum: [
+      'ponty',
+      'harcsa',
+      'csuka',
+      'keszeg',
+      'süllő',
+      'amur',
+      'compó',
+      'afrikai harcsa',
+      'busa',
+      'jász',
+      'paduc'
+    ]
+  },
+  water_depth: {
+    type: String
+  },
+  steg_number: {
+    type: String
+  },
+  day_ticket: {
+    type: [String],
+    type: {
+      type: String,
+      enum: [
+        "felnőtt",
+        "gyermek",
+        "éjszakai",
+        "diák",
+        "családi"
+      ]
+    },
+    price: {
+      type: Number
+    }
+  },
+  opening_hours: [
+    {
+      day: {
+        type: String
+      },
+      open: {
+        type: String
+      },
+      close: {
+        type: String
+      }
+    }
+  ]
+},
+)
+
+
+module.exports = mongoose.model("TavakModel", ToSchema, "lake");
