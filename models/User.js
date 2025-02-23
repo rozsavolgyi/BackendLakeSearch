cffe99def0b5764fb5df0574be5a9e72453d842d
rozsavolgyi <simonrozsavolgyi5@gmail.com>
2025-02-23 20:34:13 +0100

    további jelszó és token beállítások

--- models/User.js
+++ models/User.js
@@ -38,3 +38,34 @@ UserSchema.pre('save', async function(next) {
     const salt = await bcrypt.genSalt(10)
     this.password = await bcrypt.hash(this.password, salt)
 })
+// A JWT aláírása és visszaadása
+UserSchema.methods.getSignedJwtToken = function () {
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE
+    })
+}
+
+// A felhasználó által megadott jelszó és 
+// az adatbázisban tárolt hashelt jelszó összehasonlítása
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+// Password token létrehozása és titkosítása
+UserSchema.methods.getResetPasswordToken = function() {
+    // Token generálása
+    const resetToken = crypto.randomBytes(20).toString('hex')
+
+    // A token hash-elése és a resetPasswordToken mező beállítása
+    this.resetPasswordToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex')
+
+    // A lejárat beállítása (10 perc)
+    this.resetPasswordExpire = Date.now() + 10 * 60 * 1000 
+
+    return resetToken
+}
+
+module.exports = mongoose.model('User', UserSchema)
