3718e81b69bbf81b3c97b9fcd41a62f9f61d02d6
rozsavolgyi <simonrozsavolgyi5@gmail.com>
2025-02-23 20:28:46 +0100

    Database-hez való csatlakozás

--- server.js
+++ server.js
@@ -4,4 +4,37 @@ require('dotenv').config() // A .env fájlt olvassa
 const cors = require('cors'); // CORS importálása
 const morgan = require('morgan')
 const fileUpload = require('express-fileupload')
+const errorHandler = require('./middleware/error')
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+const mongoString = process.env.DATABASE_URL;
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+console.log('MongoDB URI:', process.env.DATABASE_URL);
 
+database.on("error", (error) => {
+    console.log(error);
+});
+
+database.once("connected", () => {
+    console.log(`Database Connected ${database.host}`);
+});
+
+
+const app = express()
+app.use(cors({
+    origin: 'http://localhost:4200', // Engedélyezi a frontendet
+    credentials: true
+}));
+
+app.use(express.json())
+app.use(morgan('dev'))
+
+app.use(fileUpload())
+
+app.use(errorHandler)
+app.get('/', (req, res) => {
+    res.status(400).json({ success: false})
+})
+
+app.listen(process.env.PORT, console.log(`Server running on port ${process.env.PORT}`));
