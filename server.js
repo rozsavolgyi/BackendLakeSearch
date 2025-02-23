c5c07d9549ae6075b2fc8f68ade6e97c03548424
rozsavolgyi <simonrozsavolgyi5@gmail.com>
2025-02-23 20:21:18 +0100

    függgőségek importálása

---
+++ server.js
@@ -0,0 +1,6 @@
+const path = require('path')
+const express = require('express')
+require('dotenv').config() // A .env fájlt olvassa
+const cors = require('cors'); // CORS importálása
+const morgan = require('morgan')
+const fileUpload = require('express-fileupload')
