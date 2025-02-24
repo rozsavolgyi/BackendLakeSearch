e2ff2b1a60a76fde050d6c7051f89816f9baf300
rozsavolgyi <simonrozsavolgyi5@gmail.com>
2025-02-24 20:14:44 +0100

    CountyModel létrehozása és feltöltése

---
+++ models/CountyModel.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const CountySchema = new mongoose.Schema({
+  name: String,
+  lakes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "TavakModel" // A TavakModel modellre történő hivatkozás
+  }]
+});
+
+module.exports = mongoose.model("CountyModel", CountySchema, "county");
