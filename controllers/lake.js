307de920d8cdc84648789fc192be6b9090868e29
rozsavolgyi <simonrozsavolgyi5@gmail.com>
2025-02-25 20:58:01 +0100

    lapozás folytatása

--- controllers/lake.js
+++ controllers/lake.js
@@ -8,11 +8,24 @@ exports.getTavak = async (req, res, next) => {
     const startIndex = (page - 1) * limit;
     const endIndex=page*limit;
     const total = await tavakModel.countDocuments();
+    const pagination = {};
+    if (startIndex > 0) {
+        pagination.prev = {
+            page: page - 1,
+            limit,
+        };
+    }
+    if (endIndex < total) {
+        pagination.next = {
+            page: page + 1,
+            limit,
+        };
+    }
 
     query=query.skip(startIndex).limit(limit);
     try {
-        const tavak = await tavakModel.find(req.query).populate({path: 'typical_fish.fish', select: '-_id'})
-        res.status(200).json({ success: true, count: tavak.length, data: tavak })
+        const tavak = await tavakModel.find(req.query).skip(startIndex).limit(limit).populate({path: 'typical_fish.fish', select: '-_id'})
+        res.status(200).json({ success: true, count: tavak.length, pagination, data: tavak })
     } catch (error) {
         res.status(500).json({ success: false })
     }
