4991d7ca3d777f0fad1db8421739839d442a3a30
rozsavolgyi <simonrozsavolgyi5@gmail.com>
2025-02-26 11:06:51 +0100

    method id alapján kéri le

--- controllers/method.js
+++ controllers/method.js
@@ -9,3 +9,16 @@ exports.getMethod=async(req,res,next)=>{
         res.status(500).json({ success: false })
     }
 }
+
+exports.getMethodById=async(req, res, next)=>{
+    try {
+        const method=await MethodModel.findById(req.params.id)
+        if (!method) {
+            return res.status(400).json({succes: false, msg:"Not found"})
+        }
+        res.status(200).json({succes: true, data: method})
+    } catch (error) {
+        next(new ErrorResponse(`Method id (${req.params.id}) not correct`, 404
+        ));
+    }
+}
