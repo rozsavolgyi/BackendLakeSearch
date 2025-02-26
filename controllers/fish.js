34cc823f5b334738f299d78c16b425ed88cbb22d
rozsavolgyi <simonrozsavolgyi5@gmail.com>
2025-02-26 11:51:23 +0100

    az összes fish lekérése

--- controllers/fish.js
+++ controllers/fish.js
@@ -1,3 +1,12 @@
+const { query } = require("express")
 const FishModel=require("../models/FishModel")
 const ErrorResponse=require("../utils/errorResponse")
 
+exports.getFish=async(req, res, next)=>{
+    try {
+        const fish=await FishModel.find(req.query)
+        res.status(200).json({success: true, count: fish.length, data: fish})
+    } catch (error) {
+        res.status(500).json({ success: false })
+    }
+}
