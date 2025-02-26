2ed077d94a0dab65e41793a37a0f5c4e68ee2d41
rozsavolgyi <simonrozsavolgyi5@gmail.com>
2025-02-26 11:02:27 +0100

    az összes method lekérése

--- controllers/method.js
+++ controllers/method.js
@@ -1,2 +1,11 @@
 const MethodModel= require("../models/MethodModel")
 const ErrorResponse=require("../utils/errorResponse")
+
+exports.getMethod=async(req,res,next)=>{
+    try {
+        const method=await MethodModel.find(req.query)
+        res.status(200).json({succes: true, count: method.length, data: method})
+    } catch (error) {
+        res.status(500).json({ success: false })
+    }
+}
