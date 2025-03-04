658095a445e966d5e70edb8924341507593ce93d
rozsavolgyi <simonrozsavolgyi5@gmail.com>
2025-03-04 18:45:49 +0100

    catch controller létrehozása

---
+++ controllers/catch.js
@@ -0,0 +1,8 @@
+const Catch=require("../models/CatchModel");
+const Fish=require("../models/FishModel");
+const Lake=require("../models/TavakModel");
+const Method=require("../models/MethodModel");
+const User=require("../models/User");
+
+
+const ErrorResponse = require("../utils/errorResponse");
