d8e823b9e1922611faf310e852690554c679d5af
rozsavolgyi <simonrozsavolgyi5@gmail.com>
2025-03-04 19:07:16 +0100

    catch router elkészítése

---
+++ routes/catch.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const {
+    getCatch,
+    getCatchById,
+    createCatch,
+    deleteCatch,
+} =require("../controllers/catch")
+
+const router = express.Router();
+
+router.get("/", getCatch);
+router.get("/:id", getCatchById);
+router.post("/", createCatch);
+router.delete("/:id", deleteCatch);
+
+module.exports = router;
