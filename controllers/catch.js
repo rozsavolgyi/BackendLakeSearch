d107aaf1cddae3b5940e832c177d325dcf00e1e0
rozsavolgyi <simonrozsavolgyi5@gmail.com>
2025-03-04 18:50:01 +0100

    getCatch létrehozása

--- controllers/catch.js
+++ controllers/catch.js
@@ -3,6 +3,14 @@ const Fish=require("../models/FishModel");
 const Lake=require("../models/TavakModel");
 const Method=require("../models/MethodModel");
 const User=require("../models/User");
-
-
 const ErrorResponse = require("../utils/errorResponse");
+
+
+ exports.getCatch=async(req,res,next)=>{
+    try{
+        const catchs=await Catch.find(req.query).populate({path: 'fish'}).populate({path: 'lake'}).populate({path: 'method'}).populate({path: 'user'})
+        res.status(200).json({success:true,count:catchs.length,data:catchs})
+    }catch(error){
+        res.status(500).json({success:false, message: 'Hiba történt a fogások lekérésekor', error})
+    }
+ }
