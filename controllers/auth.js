3acc40edb73f126f1cbb53aaea203a6e186d846e
rozsavolgyi <simonrozsavolgyi5@gmail.com>
2025-03-10 10:26:47 +0100

    Forgotpassword megírása

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,7 +11,7 @@ exports.register = async (req, res, next) => {
     const { name, email, password, role } = req.body
 
     //Ellenőrizzük, hogy a felhasználó már regisztrálva van-e
-    const existUser=await User.findOne({email})
+    const existUser = await User.findOne({ email })
     if (existUser) {
       return next(new ErrorResponse('Az email cím már regisztrálva van!', 400))
     }
@@ -75,139 +75,115 @@ exports.login = async (req, res, next) => {
 };
 
 exports.updateDetails = async (req, res, next) => {
-    try {
-      const fieldsToUpdate = {
-        name: req.body.name,
-        email: req.body.email
-      }
-  
-      const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
-        new: true,
-        runValidators: true
-      })
-  
-      res.status(200).json({
-        success: true,
-        data: user,
-      })
-    } catch (error) {
-      res.status(400).json({ success: false, msg: error.message })
+  try {
+    const fieldsToUpdate = {
+      name: req.body.name,
+      email: req.body.email
     }
+
+    const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+      new: true,
+      runValidators: true
+    })
+
+    res.status(200).json({
+      success: true,
+      data: user,
+    })
+  } catch (error) {
+    res.status(400).json({ success: false, msg: error.message })
   }
-  
-  // @desc   A bejelentkezett felhasználó jelszavának frissítése
-  // @route  PUT /auth/updatepassword
-  // @access Private
-  exports.updatePassword = async (req, res, next) => {
-    try {
-      const user = await User.findById(req.user.id).select('+password')
-  
-      // Az aktuális jelszó ellenőrzése
-      if (!(await user.matchPassword(req.body.currentPassword))) {
-        return next(new ErrorResponse('A jelszó helytelen', 401))
-      }
-  
-      user.password = req.body.newPassword
-      await user.save()
-  
-      sendTokenResponse(user, 200, res)
-    } catch (error) {
-      res.status(400).json({ success: false, msg: error.message })
+}
+
+// @desc   A bejelentkezett felhasználó jelszavának frissítése
+// @route  PUT /auth/updatepassword
+// @access Private
+exports.updatePassword = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user.id).select('+password')
+
+    // Az aktuális jelszó ellenőrzése
+    if (!(await user.matchPassword(req.body.currentPassword))) {
+      return next(new ErrorResponse('A jelszó helytelen', 401))
     }
+
+    user.password = req.body.newPassword
+    await user.save()
+
+    sendTokenResponse(user, 200, res)
+  } catch (error) {
+    res.status(400).json({ success: false, msg: error.message })
   }
-  
-  // @desc   Elfelejtett jelszó
-  // @route  POST /auth/forgotpassword
-  // @access Public
-  exports.forgotPassword = async (req, res, next) => {
-    try {
-      const user = await User.findOne({ email: req.body.email })
-  
-      if (!user) {
-        return next(new ErrorResponse('Ehhez az email-hez nem tartozik felhasználó', 404))
-      }
-  
-      // A reset token megszerzése
-      const resetToken = user.getResetPasswordToken()
-  
-      await user.save({ validateBeforeSave: false })
-  
-      // A visszaállítási URL létrehozása
-      const resetUrl = `${req.protocol}://${req.get('host')}/api/auth/resetpassword/${resetToken}`
-  
-      const message = `Azért kaptad ezt az emailt, mert egy kérés érkezett hozzánk a jelszavad visszaállítására. A jelszó visszaállításához kattints az alábbi linkre: \n\n ${resetUrl}`
-  
-      try {
-        await sendEmail({
-          email: user.email,
-          subject: 'Password reset token',
-          message
-        })
-        res.status(200).json({
-          success: true,
-          data: 'Email elküldve',
-        })
-      } catch (error) {
-        console.log(err);
-        user.resetPasswordToken = undefined
-        user.resetPasswordExpire = undefined
-  
-        await user.save({ validateBeforeSave: false })
-  
-        return next(new ErrorResponse('Email nem lett elküldve', 500))
-      }
-    } catch (error) {
-      res.status(400).json({ success: false, msg: error.message })
+}
+
+// @desc   Elfelejtett jelszó
+// @route  POST /auth/forgotpassword
+// @access Public
+exports.forgotPassword = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email })
+    if (!user) {
+      return next(new ErrorResponse('Ehhez az email-hez nem tartozik felhasználó', 404))
     }
+    // A reset token megszerzése
+    const resetToken = user.getResetPasswordToken()
+    await user.save({validateBeforeSave: false})
+    res.status(200).json({
+      success: true,
+      data: user,
+    })
+  } catch (error) {
+    res.status(400).json({ success: false, msg: error.message })
   }
-  
-  // @desc   A jelszó visszaállítása
-  // @route  PUT /api/auth/resetpassword/:resettoken
-  // @access Public
-  exports.resetPassword = async (req, res, next) => {
-    try {
-      // A hash-elt token megszerzése
-      const resetPasswordToken = crypto
+}
+
+// @desc   A jelszó visszaállítása
+// @route  PUT /api/auth/resetpassword/:resettoken
+// @access Public
+exports.resetPassword = async (req, res, next) => {
+  try {
+    // A hash-elt token megszerzése
+    const resetPasswordToken = crypto
       .createHash('sha256')
       .update(req.params.resettoken)
       .digest('hex')
-  
-      const user = await User.findOne({
-        resetPasswordToken,
-        resetPasswordExpire: { $gt: Date.now() }
-      })
-  
-      if (!user) {
-        return next(new ErrorResponse('Invalid token', 400))
-      }
-  
-      // Az új jelszó beállítása
-      user.password = req.body.password
-      user.resetPasswordToken = undefined
-      user.resetPasswordExpire = undefined
-      await user.save()
-  
-      sendTokenResponse(user, 200, res)
-    } catch (error) {
-      res.status(400).json({ success: false, msg: error.message })
-    }
-  }
-  
-  // Token megszerzése a modeltől, süti létrehozása és válasz küldése
-  const sendTokenResponse = (user, statusCode, res) => {
-    // Token létrehozása
-    const token = user.getSignedJwtToken()
-  
-    const options = {
-      expires: new Date(
-        Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000,
-      ),
-      httpOnly: true,
-      secure: false,
-    }
-  
-    res.status(statusCode).cookie('token', token, options).json({
-      success: true,
-      token,
+
+    const user = await User.findOne({
+      resetPasswordToken,
+      resetPasswordExpire: { $gt: Date.now() }
     })
+
+    if (!user) {
+      return next(new ErrorResponse('Invalid token', 400))
+    }
+
+    // Az új jelszó beállítása
+    user.password = req.body.password
+    user.resetPasswordToken = undefined
+    user.resetPasswordExpire = undefined
+    await user.save()
+
+    sendTokenResponse(user, 200, res)
+  } catch (error) {
+    res.status(400).json({ success: false, msg: error.message })
   }
+}
+
+// Token megszerzése a modeltől, süti létrehozása és válasz küldése
+const sendTokenResponse = (user, statusCode, res) => {
+  // Token létrehozása
+  const token = user.getSignedJwtToken()
+
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000,
+    ),
+    httpOnly: true,
+    secure: false,
+  }
+
+  res.status(statusCode).cookie('token', token, options).json({
+    success: true,
+    token,
+  })
+}
--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { 
     register, 
     login,
-    // forgotPassword,
+    forgotPassword,
     resetPassword,
     updateDetails,
     updatePassword
@@ -16,7 +16,7 @@ router.post('/register', register)
 router.post('/login', login)
 router.put('/updatedetails', protect,  updateDetails)
 router.put('/updatepassword', protect,  updatePassword)
-// router.post('/forgotpassword', forgotPassword)
+router.post('/forgotpassword', forgotPassword)
 router.put('/resetpassword/:resettoken', resetPassword)
 
 module.exports = router
